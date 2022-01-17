409fd214abd9ee5fe9ed248a5f877b47abd03943
Sumedh Bhat <sumedhbhat01@gmail.com>
2022-01-17 21:17:33 +0530

    Added Carousel

---
+++ src/components/Dashboard/Carousel.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import logo1 from "./assets/1.jpeg";
+import logo2 from "./assets/2.jpeg";
+import logo3 from "./assets/3.jpeg";
+import "../styles/Carousel.css";
+export default () => (
+  <Carousel
+    autoPlay={false}
+    fade
+    infiniteLoop
+    transitionTime={"500ms"}
+    showThumbs={false}
+    showStatus={false}
+    showIndicators={false}
+  >
+    <div>
+      <img alt='' className='center' src={logo1} />
+    </div>
+    <div>
+      <img alt='' src={logo2} className='center' />
+    </div>
+    <div>
+      <img alt='' src={logo3} />
+    </div>
+  </Carousel>
+);
